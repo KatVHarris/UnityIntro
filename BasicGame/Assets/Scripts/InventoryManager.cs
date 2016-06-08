7e8c02e1cfedd2ef75f1bf67a15bea4c98a9d260
Katherine Harris <kvm.harris@gmail.com>
2016-06-08 16:02:33 -0700

    Major Updates. Update project to Unity version 5.3.4f. Reconfigured the terrain and other obsolete methods.

---
+++ BasicGame/Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InventoryManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ BasicGame/Assets/Scripts/Shoot.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	public GameObject fireballPrefab; //bulletPrefab; 
+	public int idk;
+    public float speed = 200f; 
+	
+	// Use this for initialization
+	void Start () {
+		Cursor.lockState = CursorLockMode.None;
+		Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1")) {
+			GameObject clonedBullet;
+			clonedBullet = Instantiate(fireballPrefab,//bulletPrefab,
+			                           transform.position,
+			                           transform.rotation) as GameObject;
+			clonedBullet.GetComponent<Rigidbody>().AddForce(clonedBullet.transform.forward * speed);
+		}
+	}
+}
